f6b5e0fec1335fb509ad9dd89eed5c6ba08cf1b2
fbellucci29 <f.bellucci29@gmail.com>
2025-09-28 18:51:18 +0200

    Delete api/hello.js

--- api/hello.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({
-    message: "API funziona!",
-    timestamp: new Date().toISOString(),
-    method: req.method
-  });
-}
